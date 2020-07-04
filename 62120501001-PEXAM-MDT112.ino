94634d5a9bf5586bbb11f4b76e23b819774c58d5
katapat <61570104+katapat@users.noreply.github.com>
2020-07-04 15:25:03 +0700

    5 complete

--- 62120501001-PEXAM-MDT112.ino
+++ 62120501001-PEXAM-MDT112.ino
@@ -1,4 +1,11 @@
 #include "CytronMotorDriver.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 CytronMD motor1(PWM_PWM, 3, 4);  
 CytronMD motor2(PWM_PWM, 10, 11);
 void setup(){
@@ -13,11 +20,21 @@ void setup(){
   motor2.setSpeed(255);
   Serial.print("Turn CW");
   delay(2500);
+  pinMode(2, INPUT_PULLUP);  
+  oled.begin(0x3C);
+  oled.clearDisplay(); 
+  oled.setCursor(20, 10);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setTextSize(3);
 
 }
 
 void loop(){
   motor1.setSpeed(0);
   motor2.setSpeed(0);
+  oled.print("Hello MDT");
+  oled.display();
+  delay(1000);
+  oled.clearDisplay();
               
 }
