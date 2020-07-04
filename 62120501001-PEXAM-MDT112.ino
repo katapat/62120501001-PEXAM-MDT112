3853108f41ace8137194952fc667443cbbcc09e6
katapat <61570104+katapat@users.noreply.github.com>
2020-07-04 15:22:22 +0700

    3 complete

--- 62120501001-PEXAM-MDT112.ino
+++ 62120501001-PEXAM-MDT112.ino
@@ -1,9 +1,19 @@
+#include "CytronMotorDriver.h"
+CytronMD motor1(PWM_PWM, 3, 4);  
+CytronMD motor2(PWM_PWM, 10, 11);
 void setup(){
   pinMode(buzzer, OUTPUT); 
   tone(8,600,400); 
   Serial.print("Hello MDT");
+  motor1.setSpeed(-255);
+  motor2.setSpeed(-255);
+  Serial.print("Turn CCW");
+  delay(2000);
+
 }
 
 void loop(){
+  motor1.setSpeed(0);
+  motor2.setSpeed(0);
               
 }
